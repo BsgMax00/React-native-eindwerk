1bdc17761ce9a010e66924b90614c502fb7eb6cb
brame <bram.engels2002@hotmail.com>
2022-03-28 12:33:25 +0200

    error -> naar main pc

---
+++ data/data.js
@@ -0,0 +1,21 @@
+export const DATACHARACTERS = [
+    {
+        id: 1,
+        name: "Belthor",
+        troop: "mage",
+        weakness: "long range attack only",
+        restrain: "Paviser",
+        recruitment: "tavern",
+        movementSpeed: 65,
+        attackRange: 250,
+        description: "Having been born blind, Belthor chose Nigel as his god. Though he may be eccentric, he is quite nice to the orphan Kieth."
+    }
+];
+export const DATAITEMS = [
+    {
+        id: 1,
+        name: "sword",
+        damage: 78,
+        hitpoints: -1
+    }
+]
